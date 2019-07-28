7c5c6a056b0f37a3c14549e82c8a65cfb8d64cf5
Lan Qi <harrycmfan@qq.com>
2019-07-28 12:47:34 +0800

    206

---
+++ linkedlist/206_reverse_linked_list.go
@@ -0,0 +1,35 @@
+package linkedlist
+
+/*
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+Follow up:
+
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+ */
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	var previousNode *ListNode
+	curr := head
+	for curr != nil {
+		NextToTravel := curr.Next
+		curr.Next = previousNode
+		previousNode = curr
+		curr = NextToTravel
+	}
+	return previousNode
+}
